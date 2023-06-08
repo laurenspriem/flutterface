5a0aaff36c0b8bafca29dbce5fcd820f78c9bcaf
laurenspriem <github_uni.fishers@aleeas.com>
2023-06-09 00:01:14 +0200

    Display results via CustomPaint

--- lib/utils/face_detection_painter.dart
+++ lib/utils/face_detection_painter.dart
@@ -1,80 +1,59 @@
-import 'dart:developer' as devtools show log;
-import 'dart:math' show max;
-
 import 'package:flutter/material.dart';
-import 'package:flutterface/utils/image.dart';
-import 'package:image/image.dart' as image_lib;
+import 'package:flutterface/services/face_detection/detection.dart';
 
-Future<Image> drawFaces(imagePath, faceDetectionResult) async {
-  final stopwatch = Stopwatch()..start();
-  // Get image
-  image_lib.Image drawnOnOriginalImage = await loadImageImage(imagePath);
+class FacePainter extends CustomPainter {
+  final List<FaceDetectionAbsolute> faceDetections;
+  final Size imageSize;
 
-  final originalImageWidth = drawnOnOriginalImage.width;
-  final originalImageHeight = drawnOnOriginalImage.height;
-  final imagesize = max(originalImageWidth, originalImageHeight);
-  final drawRectThickness = imagesize ~/ 100;
-  final drawCircleRadius = imagesize ~/ 100;
+  FacePainter({required this.faceDetections, required this.imageSize});
 
-  // Draw faces
-  for (final detection in faceDetectionResult) {
-    // Draw bounding box as rectangle
-    drawnOnOriginalImage = image_lib.drawRect(
-      drawnOnOriginalImage,
-      x1: detection.xMinBox,
-      y1: detection.yMinBox,
-      x2: detection.xMaxBox,
-      y2: detection.yMaxBox,
-      color: image_lib.ColorFloat16.rgb(0, 0, 255),
-      thickness: drawRectThickness,
-    );
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint boundingBoxPaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3.0
+      ..color = Colors.blue;
 
-    // Draw face landmarks as circles
-    drawnOnOriginalImage = image_lib.fillCircle(
-      drawnOnOriginalImage,
-      x: detection.leftEye[0],
-      y: detection.leftEye[1],
-      radius: drawCircleRadius,
-      color: image_lib.ColorFloat16.rgb(255, 0, 0),
-    );
-    drawnOnOriginalImage = image_lib.fillCircle(
-      drawnOnOriginalImage,
-      x: detection.rightEye[0],
-      y: detection.rightEye[1],
-      radius: drawCircleRadius,
-      color: image_lib.ColorFloat16.rgb(255, 0, 0),
-    );
-    drawnOnOriginalImage = image_lib.fillCircle(
-      drawnOnOriginalImage,
-      x: detection.nose[0],
-      y: detection.nose[1],
-      radius: drawCircleRadius,
-      color: image_lib.ColorFloat16.rgb(255, 0, 0),
-    );
-    drawnOnOriginalImage = image_lib.fillCircle(
-      drawnOnOriginalImage,
-      x: detection.mouth[0],
-      y: detection.mouth[1],
-      radius: drawCircleRadius,
-      color: image_lib.ColorFloat16.rgb(255, 0, 0),
-    );
-    drawnOnOriginalImage = image_lib.fillCircle(
-      drawnOnOriginalImage,
-      x: detection.leftEar[0],
-      y: detection.leftEar[1],
-      radius: drawCircleRadius,
-      color: image_lib.ColorFloat16.rgb(255, 0, 0),
-    );
-    drawnOnOriginalImage = image_lib.fillCircle(
-      drawnOnOriginalImage,
-      x: detection.rightEar[0],
-      y: detection.rightEar[1],
-      radius: drawCircleRadius,
-      color: image_lib.ColorFloat16.rgb(255, 0, 0),
-    );
+    double scaleY = 1;
+    double scaleX = 1;
+    if (imageSize.height > 400) {
+      scaleY = 400 / imageSize.height;
+      scaleX = scaleY;
+    }
+
+    for (var face in faceDetections) {
+      // Draw bounding box
+      canvas.drawRect(
+        Rect.fromLTRB(
+          face.xMinBox.toDouble() * scaleX,
+          face.yMinBox.toDouble() * scaleY,
+          face.xMaxBox.toDouble() * scaleX,
+          face.yMaxBox.toDouble() * scaleY,
+        ),
+        boundingBoxPaint,
+      );
+
+      // Draw keypoints
+      final Paint keypointPaint = Paint()
+        ..style = PaintingStyle.fill
+        ..color = Colors.red;
+
+      final allKeypoints = face.allKeypoints;
+      for (var keypoint in allKeypoints) {
+        canvas.drawCircle(
+          Offset(
+            keypoint[0].toDouble() * scaleX,
+            keypoint[1].toDouble() * scaleY,
+          ),
+          4.0,
+          keypointPaint,
+        );
+      }
+    }
   }
-  final image = convertToFlutterImage(drawnOnOriginalImage);
-  stopwatch.stop();
-  devtools.log('drawFaces() executed in ${stopwatch.elapsedMilliseconds}ms');
-  return image;
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
 }
