deee22f68f8bc3d103fce2df1697ae8f74f54b09
laurenspriem <github_uni.fishers@aleeas.com>
2023-06-28 10:32:25 +0200

    Unit testing for `SimilarityTransform.estimate()`

--- test/similarity_transform_test.dart
+++ test/similarity_transform_test.dart
@@ -1,9 +1,10 @@
 import 'dart:developer' as devtools show log;
 
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutterface/services/face_alignment/similarity_transform_ml_linalg.dart';
+import 'package:flutterface/services/face_alignment/similarity_transform.dart';
 import 'package:ml_linalg/linalg.dart';
+import 'package:test/test.dart';
 
+// run `dart test test/similarity_transform_test.dart ` to test
 void main() {
   final lmk = [
     <double>[103, 114],
@@ -23,17 +24,26 @@ void main() {
   final expectedParameters = Matrix.fromList([
     [0.81073804, -0.05217403, -39.88931937],
     [0.05217403, 0.81073804, -46.62302376],
-    [0, 0, 1]]);
-  for (var i = 0; i < parameters.rowCount; i++) {
-    for (var j = 0; j < parameters.columnCount; j++) {
-      final actual = parameters[i][j];
-      final expected = expectedParameters[i][j];
-      devtools.log('actual: $actual, expected: $expected');
-      // test('Test parameter estimation of SimilarityTransform', () {
-      //   expect(actual, closeTo(expected, 1e-3));
-      // });
-    }
-  }
+    [0, 0, 1]
+  ]);
 
-  devtools.log('isNoNanInParam: $isNoNanInParam');
+  group('Similarity Transform Test', () {
+    for (var i = 0; i < parameters.rowCount; i++) {
+      for (var j = 0; j < parameters.columnCount; j++) {
+        final actual = parameters[i][j];
+        final expected = expectedParameters[i][j];
+        devtools.log('actual: $actual, expected: $expected');
+        test(
+            'Test parameter estimation of SimilarityTransform at [$i, $j] in parameter matrix',
+            () {
+          expect(actual, closeTo(expected, 1e-4));
+        });
+      }
+    }
+
+    devtools.log('isNoNanInParam: $isNoNanInParam');
+    test('isNoNanInParam test', () {
+      expect(isNoNanInParam, isTrue);
+    });
+  });
 }
